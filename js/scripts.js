57458d18bbc801e67bf92cb42ca603567b73c71a
Amy Churchwell <amy.churchwell@gmail.com>
2017-10-20 09:59:04 -0700

    fix indentation add more questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
   $("form#suggester").submit(function(event) {
-    var size = $("input:radio[name=biz-size]:checked").val();
+    var size = parseInt($("input:radio[name=biz-size]:checked").val());
+    var tech = parseInt($("input:radio[name=tech]:checked").val());
+    var answer = size + tech
 
-    if (size === "large") {
+    if (answer <= 2){
       $("#C").show();
       $('#RUBY').hide();
       $('#PHP').hide();
-    } else if (size === "small") {
+    } else if (answer > 2) {
       $('#RUBY').show();
       $('#PHP').hide();
       $('#C').hide();
-    } else if (size === "fast") {
-      $('#PHP').show();
-      $('#RUBY').hide();
-      $('#C').hide();
+
     }
     event.preventDefault();
   });
