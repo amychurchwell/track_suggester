f1c4fbcc08026bf606dd605907ab736feabd7404
Amy Churchwell <amy.churchwell@gmail.com>
2017-10-20 14:27:16 -0700

    debug array sort function

--- js/scripts.js
+++ js/scripts.js
@@ -5,49 +5,53 @@ $(document).ready(function() {
     var flaw = parseInt($("input:radio[name=flaw]:checked").val());
     var pro = parseInt($("input:radio[name=pro]:checked").val());
     var company = parseInt($("input:radio[name=company]:checked").val());
-
     var list = [size, tech, company, flaw, pro];
     var answer = size + tech + flaw + pro + company;
 
-debugger;
-    function sort(list) {
-      var a = [], b = []
-      for ( var i = 0; i < arr.length; i++ ) {
-        if ( list[i] !== prev ) {
-          a.push(list[i]);
-          b.push(1);
-        } else {
-            b[b.length-1]++;
-        }
-        prev = list[i];
-    }
+    debugger;
 
-    return [a, b];
+
+    /*function sort(list) {
+        var a = [], b = []
+        for ( var i = 0; i < arr.length; i++ ) {
+          if ( list[i] !== prev ) {
+            a.push(list[i]);
+            b.push(1);
+          } else {
+              b[b.length-1]++;
+          }
+          prev = list[i];
+      }
+
+      return [a, b];
+  }
+
+    var result = sort(list);
+
+    if(sort){
+
+    $("#test").append(result[1]);
 }
-
-var result = sort(list);
-document.write('[' + result[0] + ']<br>[' + result[1] + ']')
-
-
+*/
 
     if (answer >= 11 && answer <= 15) {
-        $('#PHP').show();
-        $('#RUBY').hide();
-        $('#C').hide();
+      $('#PHP').show();
+      $('#RUBY').hide();
+      $('#C').hide();
 
-    } else if (answer >= 8 && answer <= 12){
-        $('#RUBY').show();
-        $('#PHP').hide();
-        $('#C').hide();
+    } else if (answer >= 8 && answer <= 12) {
+      $('#RUBY').show();
+      $('#PHP').hide();
+      $('#C').hide();
 
-      } else if (answer < 10){
-        $("#C").show();
-        $('#RUBY').hide();
-        $('#PHP').hide();
+    } else if (answer < 10) {
+      $("#C").show();
+      $('#RUBY').hide();
+      $('#PHP').hide();
 
-      } else {
-        alert('Please make some selections.')
-      }
+    } else {
+      alert('Please make some selections.')
+    }
 
     event.preventDefault();
   });
