953ee205f6601d4d51bb95df38564ef9b08265f9
Amy Churchwell <amy.churchwell@gmail.com>
2017-10-20 09:17:13 -0700

    change question to multiple choice and edit JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   $("form#suggester").submit(function(event) {
-    var business = $("select#biz-sof").val();
+    var size = $("input:radio[name=biz-size]:checked").val();
 
-    if (business === "true") {
+    if (size === "large") {
       $("#C").show();
+      $('#RUBY').hide();
+      $('#PHP').hide();
+    } else if (size === "small") {
+      $('#RUBY').show();
+      $('#PHP').hide();
+      $('#C').hide();
+    } else if (size === "fast") {
+      $('#PHP').show();
+      $('#RUBY').hide();
+      $('#C').hide();
     }
-
     event.preventDefault();
   });
 });
